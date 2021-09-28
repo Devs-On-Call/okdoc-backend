cbe58906706d210f958cecedf730c58374554779
elenaKonstantinidi <konstantinidielena2@gmail.com>
2021-09-28 15:59:32 +0300

    Add GET /api/patients/:patientId/diagnoses endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { createTokenHandler } from "./controller/token.controller";
 import validateRequest from "./middleware/validateRequest";
 import validateToken from "./middleware/validateToken";
 import { createTokenSchema } from "./validation/token.schema";
+import { getPatientDiagnosesHandler} from "./controller/diagnoses.controller";
+
 
 export default function (app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
@@ -13,4 +15,8 @@ export default function (app: Express) {
 
     // Profile
     app.get("/api/patients/:patientId", validateToken, getPatientHandler);
+
+    //diagnoses
+    app.get("/api/patients/:patientId/diagnoses", validateToken, getPatientDiagnosesHandler);
+   
 }
