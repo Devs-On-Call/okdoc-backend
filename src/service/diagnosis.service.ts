5b72f2cf40284106589b9442021d3e0f8f3a35c3
jimskretas <jimskretas@gmail.com>
2021-10-04 10:06:36 +0300

    Sort responses of diagnoses, prescriptions & appointments

--- src/service/diagnosis.service.ts
+++ src/service/diagnosis.service.ts
@@ -4,6 +4,7 @@ import Doctor from "../model/doctor.model";
 export async function findPatientDiagnoses(id: string) {
     try {
         const diagnoses = await Diagnose.find({ patient: id }, '-patient ')
+            .sort("-date")
             .populate({ path: "doctor", model: Doctor });
         return diagnoses;
     } catch (e: any) {
--- src/service/prescription.service.ts
+++ src/service/prescription.service.ts
@@ -4,7 +4,10 @@ import Prescription from '../model/prescription.model';
 
 export async function findPrescriptions(id: string) {
     try {
-        const prescriptions = await Prescription.find({ patient: id }, '-patient').populate({ path: "doctor", model: Doctor }).populate({ path: 'diagnosis', model: Diagnosis, select: { patient: 0, doctor: 0 } });
+        const prescriptions = await Prescription.find({ patient: id }, '-patient')
+            .sort("-date")
+            .populate({ path: "doctor", model: Doctor })
+            .populate({ path: 'diagnosis', model: Diagnosis, select: { patient: 0, doctor: 0 } });
         return prescriptions;
     } catch (e: any) {
         return false;
