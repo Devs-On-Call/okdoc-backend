8570e708c174c2acc91dcda0bf881163658ca073
Mitro <mitrontatidis@gmail.com>
2021-10-06 14:47:57 +0300

    Return professions as list instead of JSON

--- src/service/profession.service.ts
+++ src/service/profession.service.ts
@@ -2,7 +2,14 @@ import Profession from "../model/profession.model";
 
 export async function getProfessions() {
     try {
-        const professions = await Profession.find({}, "-_id");
+        const professions = await Profession.find({}, "-_id").distinct("name");
+        /*array.forEach(element => professions.values{
+            professio
+        });
+        const professionsList = <string[]>JSON.parse(professions);
+        var query = Profession.find({}, "-_id");
+        query.exec()
+*/
         return professions;
     } catch (e: any) {
         return false;
