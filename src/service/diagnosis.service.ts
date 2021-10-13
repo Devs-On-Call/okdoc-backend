503f3b1a76ac277ae0bfd0678f2b106da551d915
jimskretas <jimskretas@gmail.com>
2021-10-13 10:19:52 +0300

    Populate profession in diagnoses and prescriptions

--- src/service/diagnosis.service.ts
+++ src/service/diagnosis.service.ts
@@ -1,11 +1,15 @@
 import Diagnose from "../model/diagnosis.model";
 import Doctor from "../model/doctor.model";
+import Profession from "../model/profession.model";
 
 export async function findPatientDiagnoses(id: string) {
     try {
         const diagnoses = await Diagnose.find({ patient: id }, '-patient ')
             .sort("-date")
-            .populate({ path: "doctor", model: Doctor });
+            .populate({
+                path: "doctor", model: Doctor,
+                populate: { path: "profession", model: Profession },
+            });
         return diagnoses;
     } catch (e: any) {
         return false;
