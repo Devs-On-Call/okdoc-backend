53a8ef363c20d60a480042c1307614ca7e6fe86f
Jim Skretas <jimskretas@gmail.com>
2021-10-13 20:24:36 +0300

    Populate profession and hospital in patient (#19)

--- src/service/patient.service.ts
+++ src/service/patient.service.ts
@@ -1,9 +1,21 @@
 import Doctor from "../model/doctor.model";
+import Hospital from "../model/hospital.model";
 import Patient, { PatientDocument } from "../model/patient.model";
+import Profession from "../model/profession.model";
 
 export async function findPatient(id: string) {
     try {
-        const patient = await Patient.findById(id).populate({ path: "familyDoctor", model: Doctor });
+        const patient = await Patient.findById(id)
+            .populate({
+                path: "familyDoctor",
+                model: Doctor,
+                populate: { path: "profession", model: Profession }
+            })
+            .populate({
+                path: "familyDoctor",
+                model: Doctor,
+                populate: { path: "hospital", model: Hospital }
+            });
         return patient;
     } catch (e: any) {
         return false;
